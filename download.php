4d0d779319c0f82da5d284e85facf5c8de7d08c1
M A WAL IKRAM <m@meezaan.net>
2024-06-09 18:30:59 +0000

    Testing new changes to awqaf site

--- download.php
+++ download.php
@@ -9,8 +9,8 @@
 // Calculate the date for the current and next Friday.
 //$friday = new DateTime('next friday');
 // Or manually specify a friday date.
-//$friday = new DateTime('2024-05-31');
-$friday = new DateTime('now', new DateTimeZone('Asia/Dubai'));
+$friday = new DateTime('2024-06-07');
+//$friday = new DateTime('now', new DateTimeZone('Asia/Dubai'));
 $date = $friday->format('d-m-Y');
 $wait = 1;
 $arrContextOptions = array(
@@ -27,25 +27,38 @@ echo "Checking for " . $friday->format('r') . "...\n";
 // Check all file formats.
 $baseUrls = ["https://mobileappapi.awqaf.gov.ae/APIS/TempUploads/KhutbaAttachment"];
 //$baseUrls = ["https://mobileappapi.awqaf.gov.ae/APIS/TempUploads/KhutbaAttachment", "https://m.awqaf.ae/Uploads/AudioFridaySermons", "https://m.awqaf.ae/uploads/Friday_speach"];
-$strings_to_check = ['-ar', '-ur', '-en'];
 $extensions = ['mp3', 'pdf', 'doc'];
 $outputDir = "downloads/";
 $downloadable  = [];
 $downloadedFiles = [];
-
 // Get sermon title
-$title = "The Blessing of Long Life";
-$title = str_replace(" ", "_", $title);
+$titleOrig = "Welcoming the Blessed Ten Days";
+$title = str_replace(" ", "_", $titleOrig);
+$titleEn = $titleOrig;
+$titleAr = "%D9%8A%D8%A7%20%D9%85%D8%B1%D8%AD%D8%A8%D8%A7%20%D8%A8%D8%A7%D9%84%D8%B9%D8%B4%D8%B18";
+$titleUr = "%D8%B9%D8%B4%D8%B1%DB%82%20%D8%B0%D9%89%20%D8%A7%D9%84%D8%AD%D9%90%D8%AC%D9%91%D9%8E%DB%81%20%D9%83%D9%89%20%D9%81%D8%B6%D9%8A%D9%84%D8%AA"; 
+$strings_to_check = ['-ar', '-ur', '-en', $titleEn.'-en', $titleAr.'-ar', $titleUr.'-ur'];
 
 foreach ($baseUrls as $baseUrl) {
     foreach ($strings_to_check as $string) {
         foreach ($extensions as $extension) {
+	    $lsplit = explode('-', $string);
+	    $lang = $lsplit[1];
             $url = "$baseUrl/$date$string.$extension";
             echo "Checking $url\n";
             $headers = get_headers($url, false, $context);
+	    //var_dump($url, $headers);
             if (strpos($headers[0], '200') !== false) {
-                $downloadable[$extension][] = $url;
+                $downloadable[$extension][] = ['url' =>  $url, 'lang' => $lang];
                 echo "Found $url\n";
+	    }
+	    $string2 = urldecode($lsplit[0]);	
+            $url2 = "$baseUrl/$string2.$extension";
+            echo "Checking $url2\n";
+            $headers = get_headers($url2, false, $context);
+            if (strpos($headers[0], '200') !== false) {
+                $downloadable[$extension][] = ['url' =>  $url2, 'lang' => $lang];
+                echo "Found $url2\n";
             }
 	    echo "Wait $wait seconds incase there is throttling\n";
 	    sleep($wait);
@@ -57,23 +70,29 @@ foreach ($baseUrls as $baseUrl) {
 //echo "\n" . @implode("\n", $downloadable['mp3']);
 //echo "\n" . @implode("\n", $downloadable['doc']);
 //echo "\n" . @implode("\n", $downloadable['pdf']);
-echo "\n";
+//echo "\n";
 
+var_dump($downloadable);
+exit;
 foreach ($downloadable as $extension => $urls) {
     foreach ($urls as $url) {
+	$lang = $url['lang'];
+	$url = $url['url'];
         $parts = explode('/', $url);
         $tParts = count($parts);
-        $datemp3 = $parts[$tParts - 1];
-        $dx = explode('.', $datemp3);
-        $date = $dx[0];
+        //$datemp3 = $parts[$tParts - 1];
+        //$dx = explode('.', $datemp3);
+        //$date = $dx[0];
         $dt = explode("-", $date);
         $day = $dt[0];
         $month = $dt[1];
         $year = str_replace("n", "", $dt[2]);
-        $language = $dt[3];
+        $language = $lang;
         $newDate = $year . '-' . $month . '-' . $day . '-' . $language;
         $downloadName =$outputDir . $extension . '/' . $newDate . '-' . $title . '.' . $extension;
         echo "Downloading $url...\n";
+	var_dump($url);
+	break;
         $a = file_get_contents($url, false, $context);
         echo "Writing file $url to disk at $downloadName...\n";
         file_put_contents($downloadName, $a);
